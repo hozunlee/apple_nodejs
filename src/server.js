e2a55f69b3161f9f7f5b8f5f05cd235c5ed6bb06
ho2yahh <ho2yahh@gmail.com>
2022-08-22 12:35:54 +0900

    feat: http & ws server connecting

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+const WebSocket = require("ws");
 const express = require("express");
 const app = express();
 
@@ -5,27 +7,34 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 app.listen(8080, function () {
     console.log("8080 연결되었습니다.");
 });
 
-const WebSocket = require("ws");
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+// why? 우리의 서버를 보이게 노출시키고, http서버 위에 ws 서버를 만들기 위해서
 
-const socket = new WebSocket.Server({
-    port: 8081,
-}); // 웹소켓 열기
+server.listen(3000, () => console.log("Listening on "));
 
-// 웹소켓으로 오는 유저 메세지 받으려면
-socket.on("connection", (ws, req) => {
-    ws.on("message", (msg) => {
-        console.log("유저가 보낸거 :" + msg);
-        ws.send("반사"); // 웹소켓으로 서버-> 유저 메세지 보내려면
-    });
-});
+// return;
+// // 코딩애플
+// const socket = new WebSocket.Server({
+//     port: 8081,
+// }); // 웹소켓 열기
 
-//ws 대신 socket.io 라이브러리 쓰면
-// 연결 끊기면 자동재접속
-// 웹소켓 접속자마다 자동 id 부여
-// 모든 웹소켓유저에게 전체 메세지 전송가능
-// 웹소켓방을 생성가능
+// // 웹소켓으로 오는 유저 메세지 받으려면
+// socket.on("connection", (ws, req) => {
+//     ws.on("message", (msg) => {
+//         console.log("유저가 보낸거 :" + msg);
+//         ws.send("반사"); // 웹소켓으로 서버-> 유저 메세지 보내려면
+//     });
+// });
+
+// //ws 대신 socket.io 라이브러리 쓰면
+// // 연결 끊기면 자동재접속
+// // 웹소켓 접속자마다 자동 id 부여
+// // 모든 웹소켓유저에게 전체 메세지 전송가능
+// // 웹소켓방을 생성가능
